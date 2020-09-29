447ded2e94996105e7deb0d1002665870aa18881
thevwathrey <70467427+thevwathrey@users.noreply.github.com>
2020-09-30 00:55:20 +0530

    Add files via upload

---
+++ src/main/java/demo/project/constants/DemoProjectPortletKeys.java
@@ -0,0 +1,11 @@
+package demo.project.constants;
+
+/**
+ * @author 10688
+ */
+public class DemoProjectPortletKeys {
+
+	public static final String DEMOPROJECT =
+		"demo_project_DemoProjectPortlet";
+
+}
